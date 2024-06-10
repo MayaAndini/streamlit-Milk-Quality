0dff902480d295d1782d0e33ccc66d166d02b53f
MayaAndini <161820818+MayaAndini@users.noreply.github.com>
2024-06-10 20:32:46 +0700

    Update stream-milkquality.py

--- stream-milkquality.py
+++ stream-milkquality.py
@@ -4,57 +4,50 @@ import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 
 # Membaca model prediksi anemia
-anemia_model = joblib.load('Coba_lagi_RF_model.sav')
+dtree = joblib.load('milkquality_model.sav')
 
 # Membaca model scaler
-scaler = joblib.load('scaler_model2.sav')
+scaler = joblib.load('Scaler.sav')
+
+# Membaca Label Encoder
+le = joblib.load('le.sav')
 
 # Judul web
-st.title('Prediksi Anemia Pada Anak Usia 6-59 Bulan')
-
-# Input pengguna
-Age_Years_input = st.text_input('Input usia anak dalam tahun')
-Sex_input = st.selectbox(
-    "Pilih jenis kelamin:",
-    ('Laki-Laki', 'Perempuan')
-)
-gender_mapping = {'Laki-Laki': 1, 'Perempuan': 0}
-Sex_y = gender_mapping[Sex_input]
+st.title('Prediksi Kualitas Susu')
 
 # Menampilkan hasil prediksi
-st.write('Anda memilih jenis kelamin:', Sex_input)
-RBC_count_in_Millions_input = st.text_input('Input nilai Red Blood Cell (RBC) (10^6/μL)')
-HGB_Alltitude_Adjusted_input = st.text_input('Input nilai Hemogoblin (g/dL) ')
-HCT_input = st.text_input('Input nilai Hematokrit (%) ')
-MCV_input = st.text_input('Input nilai Mean Corpuscular Volume (MCV) (fL)')
-MCH_input = st.text_input('Input nilai Mean Corpuscular Hemoglobin (MCH) (pg)')
-MCHC_input = st.text_input('Input nilai Mean Corpuscular Hemoglobin Concentration (MCHC) (g/dL)')
-RDW_input = st.text_input('Input nilai Red Cell Distribution Width (RDW) (%)')
+pH_input = st.text_input('Input Nilai pH')
+Temprature_input = st.text_input('Input nilai Temprature ')
+Taste_input = st.text_input('Input Taste ')
+Odor_input = st.text_input('Input nilai Odor')
+Colour_input = st.text_input('Input nilai Colour')
+Lemak_input = st.text_input('Input nilai Lemak')
+Turbidity_input = st.text_input('Input nilai Turbidity')
 
 # Validasi input
-if Age_Years_input.strip() and Sex_input.strip() and RBC_count_in_Millions_input.strip() and HGB_Alltitude_Adjusted_input.strip() and HCT_input.strip() and MCV_input.strip() and MCH_input.strip() and MCHC_input.strip() and RDW_input.strip():
-    Age_Years = float(Age_Years_input)
-    Sex = float(Sex_y)
-    RBC_count_in_Millions = float(RBC_count_in_Millions_input)
-    HGB_Alltitude_Adjusted = float(HGB_Alltitude_Adjusted_input)
-    HCT = float(HCT_input)
-    MCV = float(MCV_input)
-    MCH = float(MCH_input)
-    MCHC = float(MCHC_input)
-    RDW = float(RDW_input)
+if pH_input.strip() and Temprature_input.strip() and Taste_input.strip() and Odor_input.strip() and Colour_input.strip() and Lemak_input.strip() and Turbidity_input.strip():
+    pH = float(pH_input)
+    Temprature = float(Temprature_input)
+    Taste = float(Taste_input)
+    Odor = float(Odor_input)
+    Colour = float(Colour_input)
+    Lemak = float(Lemak_input)
+    Turbidity = float(Turbidity_input)
 
     # Code untuk prediksi
     # Membuat tombol untuk prediksi
-    if st.button('Test Prediksi Anemia'):
-        input_data = np.array([Age_Years, Sex, RBC_count_in_Millions, HGB_Alltitude_Adjusted, HCT, MCV, MCH, MCHC, RDW]).reshape(1, -1)
-        anem_prediction = anemia_model.predict(input_data)
+    if st.button('Test Prediksi Kualitas Susu'):
+        input_data = np.array([pH, Temprature, Taste, Odor, Colour, Lemak, Turbidity]).reshape(1, -1)
+        milkquality_prediction = dtree.predict(input_data)
 
         # Menampilkan hasil prediksi
-        if anem_prediction[0] == 1:
-            anem_diagnosis = 'Anak tidak terkena Anemia'
-            st.success(anem_diagnosis)
+if Prediksi_Susu[0] == 0:
+            Prediksi_Susu = "high"
+        elif Prediksi_Susu[0] == 1:
+            Prediksi_Susu = "low"
+        elif Prediksi_Susu[0] == 2:
+            Prediksi_Susu = "medium"
         else:
-            anem_diagnosis = 'Anak terkena Anemia'
-            st.error(anem_diagnosis)
+            Prediksi_Susu = "tidak ditemukan jenis kualitas susu"
 else:
     st.warning('Mohon lengkapi semua kolom input.')
